32a0acbbfa8d42a1adcd04e0d0863925eac3d7d6
mikeck <michael.koohafkan@gmail.com>
2014-07-16 13:44:10 -0700

    problem with setting options on package load is fixed (hopefully)

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,15 @@
 .onLoad = function(libname, pkgname){ 
   # default options
-  opts_knit$set(kfigr.link=TRUE, kfigr.prefix=FALSE)
+  eval(opts_knit$set(kfigr.link=TRUE, kfigr.prefix=FALSE), 
+       envir=getNamespace('knitr'))
   # anchor hook definition
-  knit_hooks$set(anchor = function(before, options, envir) {
-    if (before){  
-      invisible(index(options$label, options$anchor))
-      paste('<a name="', options$label, '"></a>', sep='')
-    }
-  })
+  eval(
+    knit_hooks$set(anchor = function(before, options, envir) {
+      if (before){  
+        invisible(index(options$label, options$anchor))
+        paste('<a name="', options$label, '"></a>', sep='')
+      }
+    }), envir=getNamespace('knitr'))
   anchorenv <- new.env(parent=getNamespace("kfigr"))
   assign("anchorenv", anchorenv, envir=getNamespace("kfigr"))
   assign("types", character(0), envir=anchorenv)
